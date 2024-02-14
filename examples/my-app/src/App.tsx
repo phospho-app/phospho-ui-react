26a06f2f9cc400133fa0ac287e15ca675b147e7b
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2024-02-13 21:15:21 -0800

    Bump version

--- examples/my-app/src/App.tsx
+++ examples/my-app/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { OpenFeedbackDrawer } from "../../../lib";
+import { OpenFeedbackDrawer } from "phospho-ui-react";
 
 function App() {
   const [feedback, setFeedback] = React.useState<string | null>(null);
